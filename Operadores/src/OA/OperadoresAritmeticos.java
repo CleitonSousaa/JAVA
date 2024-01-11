d848ceab403a60b99740c863aa22624dbd45d44f
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-11 10:36:07 -0300

    To Save
    Unary Operators

--- Operadores/src/OA/OperadoresAritmeticos.java
+++ Operadores/src/OA/OperadoresAritmeticos.java
@@ -6,8 +6,18 @@ public class OperadoresAritmeticos {
 		int n1 = 3;
 		int n2 = 5;
 		float m = (n1 + n2) / 2;
+		int numeroIncremento = 5;
+		int numeroDecremento = 5;
+		int valor;
+		
+		numeroIncremento++;
+		numeroDecremento--;
+		valor = 5 + numeroIncremento++;
 		
 		System.out.println("A média é igual a " + m);
+		System.out.println("Número incrementado é: " + numeroIncremento);
+		System.out.println("Número decrementado é: " + numeroDecremento);
+		System.out.println("Valor é: " + valor);
 
 	}
 
