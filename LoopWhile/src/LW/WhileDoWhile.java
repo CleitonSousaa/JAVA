8f3752d3cf6e3983c8549987ed69d7b14f246bbf
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-18 10:18:10 -0300

    To Save 18/01/2024
    while and do while repetition structure

---
+++ LoopWhile/src/LW/WhileDoWhile.java
@@ -0,0 +1,22 @@
+package LW;
+
+public class WhileDoWhile {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 5) {
+		    System.out.println(i);
+		    i++;
+		}
+		
+		
+		int a = 0;
+		do {
+		    System.out.println(a);
+		    a++;
+		} while (a < 7);
+
+
+	}
+
+}
